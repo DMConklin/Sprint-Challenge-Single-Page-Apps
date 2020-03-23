7ae984ebab3cabc9a47511d842148c4a35009fdc
Darrell-Conklin <dmconklin84@gmail.com>
2020-03-22 21:27:30 -0500

    styled h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { StyledNavUl, StyledLink } from './style';
+import { StyledH1, StyledH1Span, StyledNavUl, StyledLink } from './style';
 
 export default function Header() {
   return (
     <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <StyledH1 className="ui center">Rick <StyledH1Span>and</StyledH1Span> Morty<br />Fan Page</StyledH1>
       <StyledNavUl>
         <li><StyledLink to="/">Home</StyledLink></li>
         <li><StyledLink to="/characters">Characters</StyledLink></li>
--- src/components/style.js
+++ src/components/style.js
@@ -5,6 +5,17 @@ import { Link } from 'react-router-dom';
 
 // Header Styles
 
+export const StyledH1 = styled.h1`
+    font-family: 'Get Schwitchy';
+    font-size: 64px;
+    color: #1f9ace;
+    text-shadow: 5px 5px 5px #7ab363;
+`;
+
+export const StyledH1Span = styled.span`
+    font-size: 32px;
+`;
+
 export const StyledNavUl = styled.ul`
     list-style: none;
     width: 100%;
